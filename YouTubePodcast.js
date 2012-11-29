b340fcb393d4bb617a953b30a94b7578a92d0821
Rob Taylor <manix84@gmail.com>
2012-11-29 23:00:37 +0000

    Adding links as comments from tabs I've been holding open to remember locations

--- YouTubePodcast.js
+++ YouTubePodcast.js
@@ -18,7 +18,19 @@
 
         // 720 download URL structure.
         // http://o-o---preferred---sn-aigeznl6---v14---lscache6.c.youtube.com/videoplayback?upn=5D0Z24HQr5A&sparams=cp%2Cgcr%2Cid%2Cip%2Cipbits%2Citag%2Cratebypass%2Csource%2Cupn%2Cexpire&fexp=906357%2C916807%2C922911%2C902541%2C922401%2C920704%2C912806%2C927201%2C925706%2C928001%2C922403%2C913546%2C913556%2C916805%2C920201%2C911116%2C901451&ms=au&expire=1354081069&itag=22&ipbits=8&gcr=gb&sver=3&ratebypass=yes&mt=1354059848&ip=81.104.38.50&mv=m&source=youtube&key=yt1&cp=U0hUSVBTT19LU0NONF9ITVNBOmxyeFJSNndFZEox&id=7eea4c5f1e59f2c5&newshard=yes&signature=7AF7DDDC16D65ED0D7F36BF1659DA3C0E82358F1.5008BEA514395E37DD76FB48544AFCFB27EB741D&title=Garrys Mod - Robo Sumo Challenge
+
+        // PIPE
+        // http://pipes.yahoo.com/pipes/pipe.info?_id=2404b1b289037c4a7f616e39986a166c
+
+        // PIPE IN USE
+        // http://pipes.yahoo.com/pipes/pipe.run?YouTubeUsername=SourceFed&format=hd&_id=2404b1b289037c4a7f616e39986a166c&_render=rss&itemLimit=200
+
+        // PIPE WITH VALUES
+        // http://pipes.yahoo.com/pipes/pipe.run?YouTubeUsername=<USERNAME>&format=<FORMAT>&_id=2404b1b289037c4a7f616e39986a166c&_render=rss&itemLimit=200
+
     };
+
+
 
 
 
